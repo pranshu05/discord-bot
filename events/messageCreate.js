57cdc23a33cc48a8a30af6cb3b7224529fe53cd4
Pranshu Patel <70943732+pranshu05@users.noreply.github.com>
2022-03-31 12:02:35 +0530

    Create messageCreate.js

---
+++ events/messageCreate.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: "messageCreate",
+    async execute(message){
+        const msg = message
+        if(!msg) return
+        if (msg.author.bot) return
+       if(msg.channel.id === 958621708707856394){
+            const thread = await channel.threads.create({
+	                        name: 'Welcome',
+	                        autoArchiveDuration: 60,
+                        })
+            console.log(`Created thread: ${thread.name} in ${message.channel}`)
+       }
+  }
+} 
