8d4622f1e6ee4f349c21d6c4ba51de611582b3b0
Pranshu Patel <70943732+pranshu05@users.noreply.github.com>
2022-03-31 11:48:52 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,36 @@
-const {REST} = require("@discordjs/rest") const {Routes} = require("discord-api-types/v9") require("dotenv").config() module.exports = { name: "ready", once: true, execute(client, commands){ console.log('ready!') let watching = [ `Developer's Hub`  ] let index = 0 setInterval(() => { if(index === watching.length) index = 0 const status = watching[index] client.user.setStatus('dnd') client.user.setActivity(status, { type: "WATCHING"}) index++ }, 7000) const CLIENT_ID = client.user.id const rest = new REST ({ version : "9" }).setToken(process.env.token); (async ()  => { try{ await rest.put(Routes.applicationCommands(CLIENT_ID), { body : commands }); console.log(`Succesfully registered commands globally ! with ${client.guilds.cache.map(person => person.memberCount).reduce(function (s, v) { return s + (v || 0) }, 0)} users and ${client.guilds.cache.size} servers `) } catch (err) { if (err)  console.error(err) } })() } }
+const {REST} = require("@discordjs/rest")
+const {Routes} = require("discord-api-types/v9")
+require("dotenv").config()
+module.exports = {
+    name: "ready",
+    once: true,
+    execute(client, commands){
+        console.log('ready!')
+        let watching = [
+            `Developer's Hub`
+       ]
+        let index = 0
+        setInterval(() => {
+        if(index === watching.length) index = 0
+        const status = watching[index]
+        client.user.setStatus('dnd')
+        client.user.setActivity(status, { type: "WATCHING"})
+        index++
+    }, 7000) 
+    const CLIENT_ID = client.user.id
+    const rest = new REST ({
+        version : "9"
+    }).setToken(process.env.token);
+
+    (async ()  => {
+        try{
+            await rest.put(Routes.applicationCommands(CLIENT_ID), {
+                body : commands
+            });
+            console.log(`Succesfully registered commands globally ! with ${client.guilds.cache.map(person => person.memberCount).reduce(function (s, v) { return s + (v || 0) }, 0)} users and ${client.guilds.cache.size} servers `)
+        } catch (err) {
+          if (err)  console.error(err)
+        }
+    })()
+    }
+}
