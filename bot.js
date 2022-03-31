cfa980338d00ba69d84f3bb56234e442e4f826d1
Pranshu Patel <70943732+pranshu05@users.noreply.github.com>
2022-03-31 11:39:16 +0530

    initial commit

---
+++ bot.js
@@ -0,0 +1,36 @@
+require("dotenv").config()
+const fs = require("fs")
+const {Client,Collection} = require("discord.js")
+const client = new Client({ 
+    intents: [
+        'GUILDS',
+        'GUILD_MEMBERS',
+        'GUILD_MESSAGES',
+        'GUILD_MESSAGE_REACTIONS',
+        'GUILD_MESSAGE_TYPING',
+        'GUILD_BANS',
+        'GUILD_EMOJIS_AND_STICKERS',
+        'GUILD_INTEGRATIONS',
+        'GUILD_INVITES',
+        'GUILD_VOICE_STATES',
+        'GUILD_PRESENCES',
+    ] 
+})
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+const commands = []
+client.commands = new Collection()
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`)
+    commands.push(command.data.toJSON())
+	client.commands.set(command.data.name, command)
+}
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'))
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`)
+    if (event.once){
+        client.once(event.name, (...args) => event.execute(...args, commands))
+    } else {
+        client.on(event.name, (...args) => event.execute(...args, commands))
+    }
+}
+client.login(process.env.token)
