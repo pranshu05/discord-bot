32874832184614ee42772ada0771da9333fb3c8f
Pranshu Patel <70943732+pranshu05@users.noreply.github.com>
2022-04-01 08:21:33 +0530

    Update guildMemberremove.js

--- events/guildMemberremove.js
+++ events/guildMemberremove.js
@@ -9,12 +9,12 @@ module.exports = {
 		const newMemberEmbed = new Discord.MessageEmbed()
 			.setColor("00FFFF")
 			.setTitle("Member left")
-			.setDescription(`${member.user} had left the srver`)
+			.setDescription(`${member.user} had left the server`)
 			.setThumbnail(member.user.displayAvatarURL())
 			.setTimestamp()
 		
 			if(member.guild.id === "754381521854398595"){
-				member.guild.channels.cache.get("862296895665340467").send({
+				member.guild.channels.cache.get("926477966555381860").send({
 					embeds: [newMemberEmbed] 
 				})
 			}
