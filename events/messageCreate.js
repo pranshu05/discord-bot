99901dfd60ba3f1744964f927b60edd7018e1608
Pranshu Patel <70943732+pranshu05@users.noreply.github.com>
2022-04-01 22:14:08 +0530

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,11 +1,11 @@
-const MessageEmbed = require('discord.js')
+const Discord = require('discord.js')
 module.exports = {
     name: "messageCreate",
     async execute(message){
         const msg = message
         if (msg.author.bot) return
         if(msg.channel.id === "958621708707856394"){
-	const introEmbed = new MessageEmbed()
+	const introEmbed = new Discord.MessageEmbed()
             .setColor('#00FFFF')
             .setTitle(`Welcome ${message.author} :wave::skin-tone-1:`)
             .setAuthor({
@@ -13,15 +13,14 @@ module.exports = {
             })
             .setTimestamp()
 	    
-	    const introThread = await message.startThread({
-            name: `intro ${message.author.name}`,
-            autoArchiveDuration: 60,
-            type: 'GUILD_PUBLIC_THREAD',
-            reason: 'intro'
-        })
-
         await message.reply({embeds: [introEmbed]}).then(sentEmbed => {
             sentEmbed.react(":wave::skin-tone-1:")
+	    message.startThread({
+	    	name: `intro ${message.author.name}`,
+            	autoArchiveDuration: 60,
+            	type: 'GUILD_PUBLIC_THREAD',
+            	reason: 'intro'
+	    })
         })
             console.log(`Created thread: ${thread.name} in ${message.channel}`)
        }
