97e73931f8d8fd49c7b7ac50f6651e59a62fdc02
Pranshu Patel <70943732+pranshu05@users.noreply.github.com>
2022-04-01 08:25:53 +0530

    Update guildMemberadd.js

--- events/guildMemberadd.js
+++ events/guildMemberadd.js
@@ -3,21 +3,28 @@ const Discord = require('discord.js')
 module.exports = {
 	name: "guildMemberAdd",
 	async execute(member) {
-		
-		console.log(member.user)
-
-		const newMemberEmbed = new Discord.MessageEmbed()
+		console.log(`${member.user.username} joined ${member.guild.name}`)
+		if(member.user.bot) return
+		const role = member.guild.roles.cache.find(x => x.name === "member")
+			if(!role){
+				return
+			}else{
+				member.roles.add(role)
+			}
+			const newMemberEmbed = new Discord.MessageEmbed()
 			.setColor("00FFFF")
 			.setTitle("New Member!")
 			.setDescription(`${member.user} has joined the server! We hope you enjoy your stay!`)
 			.setThumbnail(member.user.displayAvatarURL())
 			.setTimestamp()
-			
+			.setFooter(`membercount : ${member.guild.members.cache.filter(member => !member.user.bot).size}`)
+			member.guild.channels.cache.get(guildSettings.welcome_channel_id).send({
+				embeds: [newMemberEmbed] 
+			})	
 		if(member.guild.id === "754381521854398595"){
 			member.guild.channels.cache.get("862296895665340467").send({
 				embeds: [newMemberEmbed] 
 			})
-		}
-			
+		}	
 	}
 }
