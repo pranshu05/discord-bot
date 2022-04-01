bea4443454735a9e3d8b666214b10ebec6758cf4
Pranshu Patel <70943732+pranshu05@users.noreply.github.com>
2022-04-01 21:50:43 +0530

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,14 +4,12 @@ module.exports = {
         const msg = message
         if(!msg) return
         if (msg.author.bot) return
-       if(msg.channel.id === 958621708707856394){
-            const thread = await channel.threads.create({
+        if(msg.channel.id === 958621708707856394){
+            const thread = await message.channel.threads.create({
 	                        name: 'Welcome',
 	                        autoArchiveDuration: 60,
                         })
             console.log(`Created thread: ${thread.name} in ${message.channel}`)
-       }else{
-       		return
        }
   }
 } 
